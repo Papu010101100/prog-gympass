ed098fd40a509c530fa6b28a569508fc391fbfe2
papulol <isidrolopezlinares@gmail.com>
2026-02-24 08:39:59 +0100

    Te lo subo ahora porque se me olvido hacerlo ayer

---
+++ src/sala.java
@@ -0,0 +1,20 @@
+public class Sala {
+    private int idSala;
+    private String descripcion;
+    public Sala(int idSala, String descripcion) {
+        this.idSala = idSala;
+        this.descripcion = descripcion;
+    }
+
+    public int getIdSala() {
+        return idSala;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public String toString() {
+        return "Sala #" + idSala + " - " + descripcion;
+    }
+}
--- src/socio.java
+++ src/socio.java
@@ -1,2 +1,21 @@
-public class socio {
+public class Socio {
+    private int idSocio;
+    private String nombre;
+
+    public Socio(int idSocio, String nombre) {
+        this.idSocio = idSocio;
+        this.nombre = nombre;
+    }
+
+    public int getIdSocio() {
+        return idSocio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String toString() {
+        return "ID Socio: " + idSocio + " | Nombre: " + nombre;
+    }
 }
